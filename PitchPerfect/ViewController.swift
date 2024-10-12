728e9384576eb08a1415dc462492608596721a45
earbajsaria <earbajsaria3@gmail.com>
2024-10-13 00:38:45 +0600

    first commit

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dashboardText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func playAudio(_ sender: Any) {
+        dashboardText.text = "Recording Started....."
+    }
+    
+    @IBAction func stopAudio(_ sender: Any) {
+        dashboardText.text = "Recording Stop!!!"
+    }
 }
 
